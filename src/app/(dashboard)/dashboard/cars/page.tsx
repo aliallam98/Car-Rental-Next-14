ced0f953e81de082a5ad2fccb37d729b10869879
aliallam98 <allam98.ali@gmail.com>
2024-03-23 02:13:26 +0200

    .

--- src/app/(dashboard)/dashboard/cars/page.tsx
+++ src/app/(dashboard)/dashboard/cars/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import AdminCarsPage from "../main-sections/product/products-table/page";
+import AdminCarsPage from "../main-sections/car/car-table/page";
 
 const CarsPage = () => {
   return (
